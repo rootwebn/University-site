c50e7f80636b6f5ead4349606099ac4e0481d7c8
rootwebn <115030511+rootwebn@users.noreply.github.com>
2023-04-25 16:43:31 +0300

    update 3

--- components/section4/section4.tsx
+++ components/section4/section4.tsx
@@ -1,4 +1,5 @@
 import { raleWay } from '@/pages/_app';
+import { section4Data } from '@/db';
 import s from '../../src/styles/sectionsStyle/section4.module.scss'
 
 const Section4 = () => {
@@ -8,29 +9,14 @@ const Section4 = () => {
         <p className={raleWay.className}>
           Copyright
         </p>
-        <div className={s.itemContent}>
-          <p className={raleWay.className}>
-            Vectors and icons by <a href="https://www.svgrepo.com" target="_blank">SVG Repo</a>
-          </p>
-        </div>
-        <hr />
-        <div className={s.itemContent}>
-          <p className={raleWay.className}>
-            <a href="https://www.vecteezy.com/vector-art/560307-happy-businessman-working-on-a-personal-computer-sitting-on-a-brown-leather-chair-behind-the-office-desk-vector-flat-style-illustration">Vector Flat style Illustration. Vectors by Vecteezy</a>
-          </p>
-        </div>
-        <hr />
-        <div className={s.itemContent}>
-          <p className={raleWay.className}>
-            <a href="https://www.pinterest.com.mx/pin/31736372359977462/">Images from Pinterest</a>
-          </p>
-        </div>
-        <hr />
-        <div className={s.itemContent}>
-          <p className={raleWay.className}>
-            <a href="https://github.com/rootwebn/University-site">Source code site</a>
-          </p>
-        </div>
+        {section4Data.map((item)=>(
+          <div className={s.itemContent} key={item.id}>
+            <p className={raleWay.className}>
+              {item.pData} <a href={item.aHref}>{item.aData}</a>
+            </p>
+            <hr />
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/db.js
+++ src/db.js
@@ -2,8 +2,6 @@ import img1Section2 from '../public/img/img2.png';
 import img2Section2 from '../public/img/img3.jpg';
 import img3Section2 from '../public/img/img4.png';
 
-
-
 export const section2Data = [
   {
     id: 0,
@@ -31,5 +29,32 @@ export const section2Data = [
     authorName: 'Made by: Stecenko Viacheslav',
     date: 'Made at: 17.04.2023',
     linkWork:'',
+  },
+];
+
+export const section4Data = [
+  {
+    id: 0,
+    pData: 'Vectors and icons by',
+    aData: 'SVG Repo',
+    aHref: 'https://www.svgrepo.com'
+  },
+  {
+    id: 1,
+    pData: '',
+    aData: 'Vector Flat style Illustration. Vectors by Vecteezy',
+    aHref: 'https://www.vecteezy.com/vector-art/560307-happy-businessman-working-on-a-personal-computer-sitting-on-a-brown-leather-chair-behind-the-office-desk-vector-flat-style-illustration'
+  },
+  {
+    id: 2,
+    pData: '',
+    aData: 'Image from Pinterest',
+    aHref: 'https://www.pinterest.com.mx/pin/31736372359977462/'
+  },
+  {
+    id: 3,
+    pData: '',
+    aData: 'Source code site',
+    aHref: 'https://github.com/rootwebn/University-site'
   },
 ];
